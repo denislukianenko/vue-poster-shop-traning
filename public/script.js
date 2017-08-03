9c9c039e3c5b78d0fcf17b64072272b21c35d746
denislukianenko <denlyk1@gmail.com>
2017-08-03 21:02:49 +0300

    )

--- public/script.js
+++ public/script.js
@@ -1,1 +1,20 @@
-console.log('It works.');
+new Vue({
+    el: "#app",
+    data: {
+        total: 0,
+        items: [
+            {title: 'Item1' },
+            {title: 'Item2' },
+            {title: 'Item3' },
+            {title: 'Item4' }
+        ],
+        cart: []
+    },
+    methods: {
+        addItem: function (index) {
+            this.total += 9;
+            this.cart.push(this.items[index]);
+
+        }
+    }
+});
