08cc11b4d617875a01748561f614cbf86c026eba
denislukianenko <denlyk1@gmail.com>
2017-08-04 22:05:45 +0300

    )

--- public/script.js
+++ public/script.js
@@ -1,25 +1,62 @@
+var PRICE = 9.99;
+
 new Vue({
     el: "#app",
     data: {
         total: 0,
         items: [
-            {title: 'Item1' },
-            {title: 'Item2' },
-            {title: 'Item3' },
-            {title: 'Item4' }
+            { id: 1, title: 'Item1' },
+            { id: 2, title: 'Item2' },
+            { id: 3, title: 'Item3' },
+            { id: 4, title: 'Item4' }
         ],
-        cart: []
+        cart: [],
+        search: ''
     },
     methods: {
+        onSubmit: function() {
+            
+        },
         addItem: function (index) {
-            this.total += 9;
+            this.total += PRICE;
             var item = this.items[index];
-            this.cart.push(
-                title: item.title,
-                qty: 1
-                
-            );
-
+            var found = false;
+            for(var i = 0; i < this.cart.length; i++) {
+                if (this.cart[i].id === item.id) {
+                    found = true;
+                    this.cart[i].qty++;
+                    break;
+                } 
+            }
+            if(!found) {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    qty: 1,
+                    price: PRICE
+                });
+            }
+        },
+        inc: function(item) {
+            item.qty++
+            this.total += PRICE
+        },
+        dec: function(item) {
+            item.qty--;
+            this.total -= PRICE;
+            if (item.qty <= 0) {
+                for(var i = 0; i < this.cart.length; i++) {
+                    if (this.cart[i].id === item.id) {
+                        this.cart.splice(i, 1);
+                        break;
+                    }
+                }
+            }
+        }
+    },
+    filters: {
+        price: function(price) {
+            return price.toFixed(2);
         }
     }
 });
